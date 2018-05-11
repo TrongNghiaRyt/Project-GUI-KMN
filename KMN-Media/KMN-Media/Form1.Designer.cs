c8d6693f6e1dd47361f3a6f512387ede4fea48c2
TrongNghiaRyt <34794845+TrongNghiaRyt@users.noreply.github.com>
2018-05-11 13:50:13 +0700

    Image
    Add image

--- KMN-Media/KMN-Media/Form1.Designer.cs
+++ KMN-Media/KMN-Media/Form1.Designer.cs
@@ -29,23 +29,36 @@
         private void InitializeComponent()
         {
             this.pnMenu = new System.Windows.Forms.Panel();
+            this.pnPlay = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // pnMenu
             // 
+            this.pnMenu.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.pnMenu.Location = new System.Drawing.Point(0, 0);
             this.pnMenu.Name = "pnMenu";
-            this.pnMenu.Size = new System.Drawing.Size(150, 450);
+            this.pnMenu.Size = new System.Drawing.Size(150, 500);
             this.pnMenu.TabIndex = 0;
+            // 
+            // pnPlay
+            // 
+            this.pnPlay.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pnPlay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnPlay.Location = new System.Drawing.Point(0, 500);
+            this.pnPlay.Name = "pnPlay";
+            this.pnPlay.Size = new System.Drawing.Size(800, 100);
+            this.pnPlay.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 561);
-
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.pnPlay);
             this.Controls.Add(this.pnMenu);
             this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -55,6 +68,7 @@
         #endregion
 
         private System.Windows.Forms.Panel pnMenu;
+        private System.Windows.Forms.Panel pnPlay;
     }
 }
 
